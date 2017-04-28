e928d18088006ba0d48f4ad9c6aff61329c64969
hubiqing01 <hubiqing01@58ganji.com>
2017-04-28 15:53:12 +0800

    0.03

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
 var pathTool = require('path')
 require("babel-polyfill")
-module.exports = function(entry) {
+module.exports = function(entryArr) {
     require('babel-core/register')({
         plugins: [require('babel-plugin-add-module-exports'), require('babel-plugin-transform-es2015-modules-commonjs')],
         presets: [require('babel-preset-es2015-node5'), require('babel-preset-stage-3')],
         babelrc: false
     })
-    require(pathTool.resolve([].concat(entry))) // this is es7 - gets transpile
+    require(pathTool.resolve.apply(null, [].concat(entryArr))) // this is es7 - gets transpile
 }
